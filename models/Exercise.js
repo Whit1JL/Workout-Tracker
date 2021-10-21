1fd8f92fe2a850f5ea1b4d0a0b35647f7e7e68c7
Jessica White <jessica2714white@gmail.com>
2021-10-21 13:47:50 -0500

    Made exercise.js in models folder

---
+++ models/Exercise.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+// const Schema = mongoose.Schema;
+
+// const WorkoutSchema = new Schema({
+//   author: String,
+//   title: String
+// });
+
+// const Book = mongoose.model("Book", BookSchema);
+
+
+
+// module.exports = Book;
