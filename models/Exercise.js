b71b093902516c573139d077b4e103692bdcd059
Jessica White <jessica2714white@gmail.com>
2021-10-21 13:56:10 -0500

    In new schema added day & exercises with key values

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,11 +1,27 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-// const Schema = mongoose.Schema;
-
-// const WorkoutSchema = new Schema({
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercises: [{
+        name: {
+            type: String,
+            trim: true,
+        },
+        type: {
+            type: String,
+            trim: true,
+        },
+        weight: {
+            type: Number,
+        },
+    }]
 //   author: String,
 //   title: String
-// });
+});
 
 // const Book = mongoose.model("Book", BookSchema);
 
