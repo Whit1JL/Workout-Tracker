f5c09cefcdd977e725cc16329a98d987f16bae64
Jessica White <jessica2714white@gmail.com>
2021-11-06 08:30:15 -0500

    App.use api.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-
-const mongoose = require("mongoose");
-const express = require("express");
-const logger = require("morgan");
+const mongoose = require('mongoose');
+const express = require('express');
+const logger = require('morgan');
 
 const PORT = 3005;
 
@@ -13,11 +12,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// db.on("error", error => {
-//     console.log("Database Error:", error);
-//   });
-  
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
 
 // placing routes in 
@@ -27,6 +21,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { u
 // });
 
 app.use(require("./routes/view.js"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
     console.log(`App is running on ${PORT}`);
